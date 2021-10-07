20e07b374e7cf1168f7fde8b776d580635ca61ed
AdrianaA <adrianaachurydiaz@hotmail.com>
2021-10-07 14:18:58 -0500

    commit 1

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <BasicLayouts>
+    <h2>Pagina Principal</h2>
+  </BasicLayouts> 
+</template>
+
+<script>
+import BasicLayouts from '../layouts/BasicLayouts.vue';
+export default {
+  name: 'Home',
+  components: {
+    BasicLayouts
+  }
+  
+};
+</script>
+
---
+++ src/views/Login.vue
@@ -0,0 +1,97 @@
+<template>
+    <BasicLayouts>
+        <div class = "login">
+            <h2>Inicie sesión</h2>
+            <form class = "ui form" @submit.prevent = "login">
+                <div class = "field">
+                    <input type= "text" placeholder="Nombre de usuario" v-model = "formData.identifier" :class="{ error: formError.identifier}"/> 
+                </div>
+                <div class = "field">                    
+                    <input type = "password" placeholder="Contraseña"  v-model = "formData.password" :class="{ error: formError.password}"/>
+                </div>
+                <button type="submit" class="ui button fluid primary" :class="{ loading }">
+                    Iniciar sesión
+                </button>
+            </form>    
+        <router-link to="/register">
+            Crear una cuenta
+        </router-link> 
+        </div> 
+    </BasicLayouts>    
+</template>
+
+<script>
+import { ref} from 'vue';
+import { useRouter } from 'vue-router';
+import * as Yup from 'yup';
+import BasicLayouts from '../layouts/BasicLayouts.vue';
+import { loginApi } from '../api/user';
+
+export default {
+    name:'Login',
+    components: {
+        BasicLayouts,
+    },
+    setup(){
+        let formData = ref({});
+        let formError = ref({});
+        let loading = ref(false);
+        const router = useRouter();
+
+        const schemaForm =Yup.object().shape({ identifier: Yup.string().required(true), password: Yup.string().required(true),});
+
+
+        const login = async () => {
+            formError.value ={};
+
+            try {
+                await schemaForm.validate(formData.value, { abortEarly: false });
+                try {
+                    const response = await loginApi(formData.value);
+                    if (!response?.jwt) throw 'El usuario o contraseña no son validos';
+                    setTokenApi(response.jwt);
+                    router.push('/');
+
+                
+                } catch (error){
+                    console.log(error);
+                }
+
+            } catch (error) {
+                error.inner.forEach((err) => {
+                    formError.value[err.path] = err.message;
+                });
+            }
+            
+        };
+
+            return{
+            formData,
+            formError,
+            loading,
+            login,
+        };
+    },   
+};
+
+</script>
+
+
+
+<style lang = "scss" scoped>
+.login {
+    text-align: center;
+    > h2 {
+        margin: 50px 0 30px 0;
+    }
+    .ui.form {
+        max-width: 300px !important;
+        margin: 0 auto;
+        margin-bottom: 10px;
+        input.error {
+            border-color: #faa;
+            background-color: #ffeded;
+        }
+    }
+}
+</style>
